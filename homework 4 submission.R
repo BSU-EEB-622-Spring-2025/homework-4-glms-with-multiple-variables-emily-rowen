ea4229a7888c2f47389da5700dcad38d61e843a0
absimler <allisonsimlerwil@boisestate.edu>
2025-03-26 22:48:32 -0600

    nice work! -ASW

--- homework 4 submission.R
+++ homework 4 submission.R
@@ -16,13 +16,17 @@ mae(mod, predictions(mod), na.rm = TRUE) # 145.841
 #I selected a negative binomial distribution because the predictor is either parasitized or not indicating only 2 possibilities. Negative binomial was chosen over the binomial distribution because the data is count data that is overdispersed according to the histogram below. We have a couple seedling observations that are over 2,000 but the majority are under 500. 
 hist(mistletoe$Seedlings)
 
+## ASW: right! A preferrable way to assess overdispersion would be with a dispersion test of some kind, as in class, but I also think there's nothing wrong with cutting straight to the negative binomial in ecology :) 
+
 ## 1b)
 plot_predictions(mod, condition = "Treatment")
-predicitons(mod, newdata = data.frame(Treatment - c("parasitized", "unparasitized"))
+predictions(mod, newdata = data.frame(Treatment = c("parasitized", "unparasitized")))
 
 #According to our model seedling recruitment was higher under trees that had been parasitized by mistletoe with an expected density of 308 seedlings as opposed to an expected 13 seedlings under unparasitized trees.There was a statistically significant value (p<0.05) between our groups of parasitized and unparasitized trees. The unparasitized group also had a negative estimate value indicating the relationship we see in the "plot_predictions" plot. 
 
 #Intercept tells me the number of seedlings on the log odds scale. A negative significant effect.
+## ASW: slope for treatment?
+
 ## 1c) 
 mod2<-glm.nb(Seedlings~Treatment*as.factor(Year), data = mistletoe)
 summary(mod2)
@@ -35,6 +39,8 @@ plot_predictions(mod2, condition = c("Year", "Treatment"))
 #Intercept is slope - Estimate: The change associated with year
 
 # There was a nearly significant effect of year on the amount of seedlings beneath parasitized trees and unparasitized trees. This variation does not seem to have an impact on our model but since it is nearly significant this could prompt us to include more variables to better explain the variation that we do see. 
+
+## ASW: How would you rephrase this to interpret the interaction itself? Nice work! 28/30
 
 ## Question 2:
 treemortality <- read.csv("treemortality.csv")
@@ -56,6 +62,8 @@ roc <- roc(treemortality$mortality
 ## 2b)
 #No they shouldn't need to incorporate tree size because of their additional sampling that randomized the likelihood of sampling small and large stems should remove the potential confounding effect that tree size had.  
 
+## good!
+
 
 ## 2c) 
 
@@ -73,3 +81,9 @@ plot_predictions(mod4, condition = c("slope","thinning"))
 
 
 #When holding the distance to road and slope at their averages tree mortality decreased from 52% in unthinned patches to 30% in thinned patches. This decrease was only a 22% change where our model with only thinning showed a decrease of approximately 50% which. Our results indicate that we overestimated the impact of thinning on tree mortality and that distance to road and slope are influencing the effect of the thinning treatments (and consequently tree mortality), so they should be included in our model. 
+
+## ASW:  Right! The key thing here is that slope and distance from roads are biasing the effect of thinning in the first model, making it appear more effective than it is because of the fact that thinning treatments are more likely to occur in locations where fire severity is already lower (closer to roads, on shallower slopes). 
+
+## Great job! 19/20
+## 47/50
+
